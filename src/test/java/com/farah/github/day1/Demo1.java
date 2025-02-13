fb8584972ebb7366a4f46a9013b0c2e42c84e797
Farah <Farah@DESKTOP-4IKORCG.lan>
2025-02-13 11:31:46 -0600

    creating remote repository

---
+++ src/test/java/com/farah/github/day1/Demo1.java
@@ -0,0 +1,23 @@
+package com.farah.github.day1;
+
+import utils.Hooks;
+
+public class Demo1 extends Hooks{
+	
+	public static void maun(String args []) {	
+	
+	setUp("edge");
+	
+	driver.get("https://ntkhr.noortecktraining.com/web/index.php/auth/login");
+	
+	
+	
+	
+	
+	
+	
+	
+	tearDown();
+
+}
+}
